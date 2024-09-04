721627f50ec8f1ddb4618b6fa659aa63624265a1
manthan-chauhan-17 <manthanchauhan062@gmail.com>
2024-09-04 14:27:20 +0530

    Onboarding Page Added

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:mind_care/theme.dart';
 
 class Onboarding extends StatelessWidget {
   const Onboarding({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              "assets/images/onboarding.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 50.0),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Text(
+                "MindCare",
+                style: AppTheme.lightTheme.textTheme.titleLarge!.copyWith(
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SizedBox(
+                height: 70.0,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppTheme.lightTheme.focusColor,
+                    minimumSize: const Size(double.infinity, 50.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                  ),
+                  child: Text(
+                    "Let us help you",
+                    style: AppTheme.lightTheme.textTheme.bodyLarge,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
